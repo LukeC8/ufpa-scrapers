fac73591acd70b16e31490cab538f7a337ce72e8
Lucas Correa <lucas.correa@itec.ufpa.br>
2017-01-14 00:15:12 -0300

    Correcao de nome das urls

--- crawlers/example.php
+++ crawlers/example.php
@@ -9,7 +9,7 @@ $url_editais = "https://www.portal.ufpa.br/imprensa/todosEditais.php";
 //------------------------------
 //resposta em JSON para Noticias
 //------------------------------
-$scraper = new ScraperNews($url);
+$scraper = new ScraperNews($url_noticias);
 
 echo $scraper->scrapePage() . "\n\n";
 
@@ -18,7 +18,7 @@ unset($scraper);
 //------------------------------
 //resposta em JSON para Eventos
 //------------------------------
-$scraper = new ScraperNews($url);
+$scraper = new ScraperNews($url_eventos);
 
 echo $scraper->scrapePage() . "\n\n";
 
@@ -27,7 +27,7 @@ unset($scraper);
 //------------------------------
 //resposta em JSON para Editais
 //------------------------------
-$scraper = new ScraperNews($url);
+$scraper = new ScraperNews($url_editais);
 
 echo $scraper->scrapePage() . "\n\n";
 
